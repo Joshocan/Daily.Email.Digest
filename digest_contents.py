499c8029277e3b9276e4116a33e4c7b4a15e695a
Joshua Tetteh Ocansey <joshocan@gmail.com>
2021-10-09 22:51:57 +0200

    Get top political news update from news api

--- digest_contents.py
+++ digest_contents.py
@@ -1,11 +1,7 @@
 from urllib import request
 import json
 import datetime
-
-
-
 class DailyDigestContents:
-
     def get_weather_forecast(self, city='Bergen', state='no'):
         try:
             with open('forecast_api.txt', 'r') as api:
@@ -17,16 +13,15 @@ class DailyDigestContents:
             sunset = datetime.datetime.fromtimestamp(data['sys']['sunset'])
 
             forecast_data = {'city': data['name'],
-                        'country': data['sys']['country'],
-                        'current_date': period,
-                        'sunrise': sunrise,
-                        'sunset': sunset,
-                        'temperature': round(data['main']['temp']),
-                        'min_temperature': round(data['main']['temp_min']),
-                        'max_temperature': round(data['main']['temp_max']),
-                        'humidity': round(data['main']['humidity']),
-                        'weather_condition': data['weather'][0]['description']
-                        }
+                             'country': data['sys']['country'],
+                             'current_date': period,
+                             'sunrise': sunrise,
+                             'sunset': sunset,
+                             'temperature': round(data['main']['temp']),
+                             'min_temperature': round(data['main']['temp_min']),
+                             'max_temperature': round(data['main']['temp_max']),
+                             'humidity': round(data['main']['humidity']),
+                             'weather_condition': data['weather'][0]['description']}
 
             return forecast_data
 #            return data_formatted
@@ -40,7 +35,6 @@ class DailyDigestContents:
 
             sport_url = f'http://newsapi.org/v2/top-headlines?country={country}&category=sports&apiKey={api_key}'
             data = json.load(request.urlopen(sport_url))
-            data_formatted = json.dumps(data, indent=4)
             top_data = dict()
             top_new = []
             for top in data['articles']:
@@ -55,28 +49,49 @@ class DailyDigestContents:
         except Exception as e:
             print(e)
 
-    def get_political_update(self):
-        pass
+    def get_political_update(self, country='gb'):
+        try:
+            with open('sport_api.txt', 'r') as api:
+                api_key = api.read()
+
+            politics_url = f'http://newsapi.org/v2/top-headlines?country={country}&category=sports&apiKey={api_key}'
+            data = json.load(request.urlopen(politics_url))
+            top_data = dict()
+            top_new = []
+            for top in data['articles']:
+                top_data.update({'source': top['source']['name'],
+                                 'title': top['title'],
+                                 'url': top['url'],
+                                 'date_published': top['publishedAt']})
+
+                top_new.append(top_data)
+
+            return json.dumps(top_new, indent=4)
+        except Exception as e:
+            print(e)
 
 
 
     def get_twitter_update(self):
-        pass
+        try:
+            pass
+        except Exception as err:
+            print(err)
 
 if __name__ == '__main__':
     city = 'Ashaiman'
     state = 'Ghana'
     forecast_data = DailyDigestContents().get_weather_forecast(city,state)
-#    print('Testing values from weather api data \n')
-#    print(forecast_data)
+    print('Testing values from weather api data \n')
+    print(forecast_data)
 
     country = 'gb'
     sport_data = DailyDigestContents().get_sport_update(country)
     print('Testing the top 20 sports headline  \n')
     print(sport_data)
 
-    politics = DailyDigestContents().get_political_update()
-#   print(politics)
+    politics = DailyDigestContents().get_political_update(country='it')
+    print(politics)
 
 
 
